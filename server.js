94a0ead62d87ac698059d330b2137369194cef16
Antonio Marino <marino.antonio375@gmail.com>
2021-11-08 22:07:48 -0500

    updating my port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Dependencies
-const express = require("express");
+var express = require("express");
 
 // I'm creating an 'express' server called app
-const app = express();
+var app = express();
 
-// Sets a port or run at 3001
-const PORT = process.env.PORT || 3001;
+// Sets a port or run at 
+var PORT = process.env.PORT || 3001;
 
 //Use a middleware to parse the JSON data
 app.use(express.urlencoded({ extended: true}));
@@ -18,5 +18,5 @@ require("./routes/htmlRoutes")(app);
 
 //Listener
 app.listen(PORT, function() {
-    console.log("App listening on http://localhost:${PORT}");
+    console.log("App listening on PORT: " + PORT);
 });
